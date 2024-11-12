4b3f14b456e1c94fd717acb80d689686929cd687
Edwin Antonio Sion Tepe <tepe1600@gmail.com>
2024-11-12 11:52:17 -0800

    Initial commit

--- index.js
+++ index.js
@@ -10,36 +10,32 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// Asegúrate de que la carpeta 'output' existe
-if (!fs.existsSync('./output')) {
-    fs.mkdirSync('./output');
-}
 
-// Enviar el archivo HTML correcto
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.hmlt'));
+})
 
 app.post('/create-pdf', (req, res) => {
     const { content } = req.body;
 
     const html = `
-        <html>
-            <head>
-                <style>
-                    body { font-family: Arial, sans-serif; padding: 20px; }
-                </style>
-            </head>
-            <body>${content}</body>
-        </html>
-    `;
+                <html>
+                    <head>
+                        <style>
+                            body { font-family: Arial, sans-serif; padding: 20px; }
+                        </style>
+                    </head>
+                    <body>${content}</body>
+                </html>
+                `;
 
     const options = { format: 'Letter' };
 
-    pdf.create(html, options).toFile('./output/document.pdf', (err, result) => {
-        if (err) return res.status(500).send('Hubo un error al crear el pdf');
+    pdf.create(html, options).toFile('./output/documento.pdf', (err, result) => {
+        
+        if(err) return res.status(500).send('Hubo un error al crear el pdf');
 
-        const downloadLink = `https://demo-document-to-qr.onrender.com/download/document.pdf`;
+        const downloadLink = `http://localhost:3000/download/document.pdf`;
 
         const qrCode = qr.imageSync(downloadLink, { type: 'png' });
 
@@ -48,16 +44,20 @@ app.post('/create-pdf', (req, res) => {
             <a href="${downloadLink}" target="_blank">Descargar PDF</a>
             <br>
             <img src="data:image/png;base64,${qrCode.toString('base64')}" alt="QR Code" />
-        `);
-    });
-});
+          `);
+
+    })
+
+})
+
 
 // Ruta para descargar el PDF
 app.get('/download/document.pdf', (req, res) => {
     const filePath = path.join(__dirname, 'output', 'document.pdf');
     res.download(filePath);
-});
+  });
+  
+  app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+  });
 
-app.listen(3000, () => {
-    console.log('Server running on https://demo-document-to-qr.onrender.com:3000');
-});
